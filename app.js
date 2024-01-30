abab82c4258a229aadf515428beb7f817a35115d
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-01-30 22:10:33 +0530

    string

---
+++ app.js
@@ -0,0 +1,159 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+// for(let i=1;i<=10;i++){
+//     console.log(i*7);
+// }
+
+// let arr = [1,2,3,4,5,6];
+// let constant = arr.values();
+// console.log(constant.next().value);
+// console.log(constant.next().value);
+// console.log(constant.next().value);
+// console.log(constant.next().value);
+// console.log(constant.next().value);
+// console.log(constant.next().value);
+
+
+
+
+// let a = 4;
+// let b = 6;
+// let c;
+
+// console.log (a);
+// console.log(b)
+
+// c =a;
+// a=b;
+// b=c;
+// console.log(a);
+// console.log(b);
+
+
+
+// let doneOfWork = false;
+// let deployment;
+
+// if(doneOfWork==true) {
+//     deployment="you can start the deployment";
+//     console.log(deployment);
+// } else {
+//     deployment = "work is not done yet";
+//     console.log(deployment);
+// }
+
+
+
+// let todaysTemp = 24;
+
+// if(todaysTemp){
+//     console.log("you can go outside without jacket")
+// } 
+// else  {
+//     console.log("use jacket in your body , outside is codld")
+// }
+
+
+
+// let temp = 31;
+// let humidity =91;
+
+// if(temp>30 && humidity>90) {
+//     console.log("you are in sea beach");
+// } else if(temp<30 && humidity<90){
+//     console.log("you are in city")
+// } else {
+//     console.log("you are in village")
+// }
+
+// let temp = 21;
+// let humidity =88;
+
+// if(temp>=30 ) {
+//     if(humidity>90) {
+//         console.log("you are in sea beach")
+//     }  if (humidity <90) {
+//         console.log("you are in city");
+//     } 
+// }
+// else {
+//         console.log("you are in village")
+//     }
+// } else if(temp<30 && humidity<90){
+//     console.log("you are in city")
+// } else {
+//     console.log("you are in village")
+// }
+
+// let n = 10;
+// let end=0;
+
+// for(let i =1;i<=n;i++) {
+// //   let _end = end +i
+//     end = end +i
+// }
+// console.log(end)
+
+
+// l
+
+
+
+
+// let arr = [8,9,10,11,12]
+// // console.log(arr[0])
+// var accumolator =0;
+// for ( let i = 0; i<=arr.length-1;i++) {
+//     // console.log(arr[i]);
+//     accumolator+=arr[i]
+// }
+// console.log(accumolator)
+
+// let array = [3,867,67,95];
+// let max = -1;
+// for (let i =0; i<=array.length-1; i++) {
+//     if (array[i] > max) {
+//         max=array[i];
+        
+//     }
+    
+// }
+// console.log(max)
+
+
+// let array = [3,867,67,95];
+// let min = 1000;
+// for (let i =0; i<=array.length-1; i++) {
+//     if (array[i] < min) {
+//         min=array[i];
+        
+//     }
+    
+// }
+// console.log(min)
+
+
+
+// let array = [1 , 2 , 3 ,4,5,6,7,8 ]
+// let even = 0;
+// for(let i =0; i<=array.length-1; i+=2) {
+//     // console.log(i)
+//     even+=i
+// }
+// console.log(even)
+
+
+
+
+
+//string
+
+// let str = "amiya";
+
+// if((str[0]==="a") && (str.length>3)) {
+//     console.log("it is a good string")
+// }
+// else {
+//     console.log("it is not a good string")
+// }
