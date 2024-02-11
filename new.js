a6a7020410bb983d25e756af57c6809f1372c70a
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-02-11 21:13:43 +0530

    added dsa more

--- new.js
+++ new.js
@@ -1,32 +1,91 @@
-let startArr = [4,55,68,92,101,124,245,456]
+// let startArr = [4,55,68,92,101,124,245,456]
 
-let valueIndex = 92;
+// let valueIndex = 92;
 
-function sum ( startArr , valueIndex , start , end) 
-{
-    while (start <= end) {
-        console.log("start is",start)
-        console.log("end is",end)
+// function sum ( startArr , valueIndex , start , end) 
+// {
+//     while (start <= end) {
+//         console.log("start is",start)
+//         console.log("end is",end)
 
-        let middle = Math.floor(( start + end ) / 2);
-        console.log("middle is",middle)
+//         let middle = Math.floor(( start + end ) / 2);
+//         console.log("middle is",middle)
     
-        if(startArr[middle] == valueIndex ){
-            return true;
-        }
-        else if(startArr[middle] < valueIndex){
-            start = middle + 1
-            // return true;
-        }
-        else {
-           end = middle -1 
-        }
-    }
-    return false
+//         if(startArr[middle] == valueIndex ){
+//             return true;
+//         }
+//         else if(startArr[middle] < valueIndex){
+//             start = middle + 1
+//             // return true;
+//         }
+//         else {
+//            end = middle -1 
+//         }
+//     }
+//     return false
     
-}
+// }
 
-console.log( sum( startArr , valueIndex , 0 , startArr.length -1))
+// console.log( sum( startArr , valueIndex , 0 , startArr.length -1))
+
+
+
+
+
+
+
+
+// Search element in array JS 
+
+
+// setTimeout( () => {
+//     console.log("tu nikal yaha se")
+// }, 5000)
+
+
+setInterval( ()=> {
+    // console.log("tu vaag yaha se")
+    console.log( new Date());
+} , 2000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
