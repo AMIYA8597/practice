6a90b556c4a04b6f7a550f37e3a214674233e40b
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-03-03 22:36:33 +0530

    input string is valid or not

--- new.js
+++ new.js
@@ -94,6 +94,61 @@
 
 
 
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+ 
+
+// Example 1:
+
+// Input: s = "()"
+// Output: true
+// Example 2:
+
+// Input: s = "()[]{}"
+// Output: true
+// Example 3:
+
+// Input: s = "(]"
+// Output: false
+
+
+
+
+
+// var isValid = function(s) {
+//     const stack = [];
+//    const mapping = {
+//        '(': ')',
+//        '[': ']',
+//        '{': '}'
+//    };
+
+//    for (let i = 0; i < s.length; i++) {
+//        const currentChar = s[i];
+
+//        if (mapping.hasOwnProperty(currentChar)) {
+//            // If it's an open bracket, push it onto the stack
+//            stack.push(currentChar);
+//        } else {
+//            // If it's a close bracket, check if it matches the top of the stack
+//            if (stack.length === 0 || mapping[stack.pop()] !== currentChar) {
+//                return false; // Mismatch, return false
+//            }
+//        }
+//    }
+
+//    return stack.length === 0;
+// };
+
+
+
+
+
 
 
 
