2809b6123b5aa9615b7d4a8c2c98eed9a5dfef2e
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-03-14 08:52:21 +0530

    array

--- array.js
+++ array.js
@@ -128,6 +128,92 @@ console.log( "max sub number is",maxSubnums( [5,-6,3,8,-12,9,4,-8]));
 
 
 
+
+
+
+
+
+// A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+// For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+// The next permutation of an array of integers is the next lexicographically greater permutation of its integer. More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+// For example, the next permutation of arr = [1,2,3] is [1,3,2].
+// Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+// While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+// Given an array of integers nums, find the next permutation of nums.
+
+// The replacement must be in place and use only constant extra memory.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: [1,3,2]
+// Example 2:
+
+// Input: nums = [3,2,1]
+// Output: [1,2,3]
+// Example 3:
+
+// Input: nums = [1,1,5]
+// Output: [1,5,1]
+
+
+
+
+
+
+// var nextPermutation = function(nums) {
+//     let i = nums.length - 2;
+//     while (i >= 0 && nums[i] >= nums[i + 1]) {
+//         i--;
+//     }
+    
+//     if (i >= 0) {
+//         let j = nums.length - 1;
+//         while (j >= 0 && nums[j] <= nums[i]) {
+//             j--;
+//         }
+//         swap(nums, i, j);
+//     }
+    
+//     reverse(nums, i + 1);
+// };
+
+// function swap(nums, i, j) {
+//     let temp = nums[i];
+//     nums[i] = nums[j];
+//     nums[j] = temp;
+// }
+
+// function reverse(nums, start) {
+//     let i = start;
+//     let j = nums.length - 1;
+//     while (i < j) {
+//         swap(nums, i, j);
+//         i++;
+//         j--;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
 
 // Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
