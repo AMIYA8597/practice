a9b2a2895a059501ef4e59776312eaaed18623cb
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-03-18 08:58:45 +0530

    array

--- array.js
+++ array.js
@@ -287,6 +287,57 @@ console.log( "max sub number is",maxSubnums( [5,-6,3,8,-12,9,4,-8]));
 
 
 
+// You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+// Increment the large integer by one and return the resulting array of digits.
+
+ 
+
+// Example 1:
+
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
+// Example 2:
+
+// Input: digits = [4,3,2,1]
+// Output: [4,3,2,2]
+// Explanation: The array represents the integer 4321.
+// Incrementing by one gives 4321 + 1 = 4322.
+// Thus, the result should be [4,3,2,2].
+// Example 3:
+
+// Input: digits = [9]
+// Output: [1,0]
+// Explanation: The array represents the integer 9.
+// Incrementing by one gives 9 + 1 = 10.
+// Thus, the result should be [1,0].
+
+
+
+
+
+
+
+// var plusOne = function(digits) {
+//     const n = digits.length;
+//     for (let i = n - 1; i >= 0; i--) {
+//         if (digits[i] < 9) {
+//             digits[i]++;
+//             return digits;
+//         } else {
+//             digits[i] = 0;
+//         }
+//     }
+//     // If all digits were 9, add a new digit with a value of 1 at the beginning
+//     return [1, ...digits];
+// };
+
+
+
+
 
 
 
