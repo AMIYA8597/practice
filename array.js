8414e5cdde5d6bd145b66f104b1c3e5703463696
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-03-26 22:18:27 +0530

    array

--- array.js
+++ array.js
@@ -144,6 +144,54 @@
 
 
 
+
+// You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+// Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+
+// 0 <= j <= nums[i] and
+// i + j < n
+// Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,3,1,1,4]
+// Output: 2
+// Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+// Example 2:
+
+// Input: nums = [2,3,0,1,4]
+// Output: 2
+
+
+
+
+// var jump = function(nums) {
+//     if (nums.length <= 1) return 0;
+
+// let jumps = 1;
+// let currMaxReach = nums[0];
+// let nextMaxReach = nums[0];
+
+// for (let i = 1; i < nums.length; i++) {
+//     if (i > currMaxReach) {
+//         jumps++;
+//         currMaxReach = nextMaxReach;
+//     }
+
+//     nextMaxReach = Math.max(nextMaxReach, i + nums[i]);
+// }
+
+// return jumps;
+// };
+
+
+
+
+
+
 // Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
 
 // You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
