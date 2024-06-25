4666b0a83d7d52111023fa311098ccc7dc76e3f1
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-06-25 13:14:58 +0530

    Reverse Integer

--- dailyCode.js
+++ dailyCode.js
@@ -1,3 +1,64 @@
+//   REVERSE INTEGER 
+
+
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+// Example 1:
+
+// Input: x = 123
+// Output: 321
+// Example 2:
+
+// Input: x = -123
+// Output: -321
+// Example 3:
+
+// Input: x = 120
+// Output: 21
+
+
+
+var reverse = function(x) {
+    let max = 2147483647;   // 2**31 -1
+    let min = -2147483648;  // -2**31
+
+    let result = 0;
+    let sign = x < 0 ? -1: 1;
+    x = Math.abs(x);
+    while (x !=0) {
+        let pop = x % 10;
+        x = Math.floor(x / 10);
+
+        if(result > (max - pop) /10) {
+            return 0;
+        }
+        result = result * 10 + pop;
+
+       
+    }
+
+     result *= sign;
+
+    return result;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Longest Palindromic Substring
 
 
