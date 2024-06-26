daf3a5989994fedc56d738e0fddb9a2a6f6b6622
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-06-26 17:18:48 +0530

    Merge Two Sorted Lists

--- dailyCode.js
+++ dailyCode.js
@@ -1,3 +1,92 @@
+// 21. Merge Two Sorted Lists
+
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+// Example 1:
+
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+// Example 2:
+
+// Input: list1 = [], list2 = []
+// Output: []
+// Example 3:
+
+// Input: list1 = [], list2 = [0]
+// Output: [0]
+
+
+
+
+
+var mergeTwoLists = function(list1, list2) {
+    let dummy = { next: null };
+    let current = dummy;
+
+    while (list1 !== null && list2 !== null) {
+        if (list1.val < list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
+
+    // If either list runs out, connect the remaining elements
+    if (list1 !== null) {
+        current.next = list1;
+    } else if (list2 !== null) {
+        current.next = list2;
+    }
+
+    return dummy.next;
+}
+
+// Helper function to convert an array to a linked list
+function arrayToList(arr) {
+    let dummy = { next: null };
+    let current = dummy;
+    for (let val of arr) {
+        current.next = { val: val, next: null };
+        current = current.next;
+    }
+    return dummy.next;
+}
+
+// Helper function to convert a linked list to an array
+function listToArray(head) {
+    let arr = [];
+    let current = head;
+    while (current !== null) {
+        arr.push(current.val);
+        current = current.next;
+    }
+    return arr;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //   REVERSE INTEGER 
 
 
